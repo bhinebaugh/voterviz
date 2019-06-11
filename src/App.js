f0a7a7b87adce90a42223f97f4e940da87e46987
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-06-10 23:32:50 -0400

    fetch vote data from files into app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { } from 'react';
 import './App.css';
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor() {
+    super();
+    this.dataset = {};
+    this.sourceFiles = {
+      parties: ['parties_2004', 'parties_2008', 'parties_2012'],
+      results: ['results_2004', 'results_2008', 'results_2012']
+    }
+  }
+
+  componentDidMount = () => {
+    this.sourceFiles.results.forEach( file =>
+      fetch('/data/'+file+'.json')
+        .then( response => response.json() )
+        .then( json => this.dataset[file] = json )
+    )
+    this.sourceFiles.parties.forEach( file =>
+      fetch('/data/'+file+'.json')
+      .then( response => response.json() )
+      .then( json => this.dataset[file] = json )
+    )
+  }
+
+  render = () => (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
