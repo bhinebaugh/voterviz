e294474d2af9b80cd9c159af35f3abba1ab5e95f
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-06-12 16:11:44 -0400

    proportional bar to represent votes received

--- src/components/StateMatchUp.js
+++ src/components/StateMatchUp.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './state-match-up.css';
 
-const StateMatchUp = ({stateName, year, voteData}) => {return (
+const StateMatchUp = ({stateName, year, voteData}) => {
+    var voteTotal = voteData.reduce( (acc,value) => acc + value.votes, 0 )
+    return (
     <div className="panel state">
-        <h2>{stateName} &mdash; {year}</h2>
+        <header>
+            <h2>{stateName} &mdash; {year}</h2>
+            <p>
+                {voteTotal} votes cast
+            </p>
+        </header>
         {voteData.map( result => 
             <li className="candidate" key={result.id+result.name}>
                 <div className="candidate-details">
@@ -13,11 +20,15 @@ const StateMatchUp = ({stateName, year, voteData}) => {return (
                     </p>
                 </div>
                 <div className="votes">
+                    <svg width="100" height="20" viewbox="0 0 100 20">
+                        <rect x="0" y="0" width={100*result.votes/voteTotal+"%"} height="10" />
+                    </svg>
                     {result.votes} votes
                 </div>
             </li>
         )}
     </div>
-)}
+    )
+}
 
 export default StateMatchUp
