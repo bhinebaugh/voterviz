65faa28d5ff292d0e64e01eedf8bd20eb2aeedf7
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-06-19 08:40:17 -0400

    adding homepage to settings

---
+++ docs/precache-manifest.70a61791ee325fa5eba0cbbc24b2e00c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b018f5b068899bfbdc0c8e6e2d3a36",
+    "url": "/voterviz/index.html"
+  },
+  {
+    "revision": "5cf4427a9f792cb8fcd6",
+    "url": "/voterviz/static/css/main.abb61fbd.chunk.css"
+  },
+  {
+    "revision": "530c17cce8ca8683144d",
+    "url": "/voterviz/static/js/2.525bcecf.chunk.js"
+  },
+  {
+    "revision": "5cf4427a9f792cb8fcd6",
+    "url": "/voterviz/static/js/main.423a68ba.chunk.js"
+  },
+  {
+    "revision": "3512a71e1028747bf0ac",
+    "url": "/voterviz/static/js/runtime~main.bb0181c8.js"
+  }
+]);
